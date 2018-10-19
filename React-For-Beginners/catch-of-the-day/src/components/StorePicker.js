19d3b4ecdcbe158300dfd90577af12166f3f6e69
Tommy <tlmeyer942@gmail.com>
2018-10-19 11:56:27 -0700

    Finishes building first component in catch of the day.

--- React-For-Beginners/catch-of-the-day/src/components/StorePicker.js
+++ React-For-Beginners/catch-of-the-day/src/components/StorePicker.js
@@ -1,5 +1,6 @@
 import React from 'react';
 
+
 class StorePicker extends React.Component {
   render() {
     return <p>Hellow!</p>
--- React-For-Beginners/catch-of-the-day/src/index.js
+++ React-For-Beginners/catch-of-the-day/src/index.js
@@ -1,3 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
+import StorePicker from './components/StorePicker.js';
 
+
+render(<StorePicker />, document.querySelector("#main"));
